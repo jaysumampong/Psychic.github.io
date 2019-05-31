36d7fd6669b5975ab5a8d44366cfb4baaa9d1e67
Meng Hao <jaysumampong@gmail.com>
2019-05-30 21:20:07 -0600

    jay

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,48 +1,71 @@
-let letterChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z']
 let wins = 0;
 let losses = 0;
 let guesses = 10;
 let array = [];
-let letters = letters;
+let letterChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z'];
+// DOM node references
+let userWins = document.getElementById("user-wins");
+let userLosses = document.getElementById("user-losses");
+let guessesLeft = document.getElementById("guesses-left");
+let recordedGuesses = document.getElementById("recorded-guesses");
 
 
-let directionsText = document.getElementById('directions-text');
-let userChoiceText = document.getElementById('userchoice-text');
-let winsText = document.getElementById('wins-text');
-let lossesText = document.getElementById('losses-text');
+userLosses.innerText = losses;
+guessesLeft.innerText = guesses;
+userWins.innerText = wins;
 
-function jsGuess() {
-    letters = letterChoices[Math.floor(Math.random() * letterChoices.length)];
-    console.log(ranLetter);
+let computerChoice = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+console.log(computerChoice);
+
+function game(){
+    computerChoice = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+};
 
 document.onkeyup = function(event) {
     let userGuess = event.key;
-    letters = letterChoices[Math.floor(Math.random() * letterChoices.length)];
-    if (userGuess === letterChoices){
+    // check for only A-Z character keys (keycode)
+    if (array.includes(userGuess)) {
+        return;
+    }
+    
+    array.push(userGuess);
+
+    console.log(array);
+    if (userGuess === computerChoice){
         wins++;
-        guesses=10;
+        userWins.innerText = wins;
+        // reset game here but don't clear wins/losses
     }
-    if (userGuess !== letters){
-        availableAttempts--;
-        console.log
+
+    if (userGuess !== computerChoice) {
+        guesses--;
+        console.log({guesses});
+        guessesLeft.innerText = guesses;
     }
-    if (availableAttempts === 0){
+
+    if (guesses <= 0){
         losses++;
         array = [];
         guesses = 10; 
     }
-    if (array.indexOf >= 0){
-    }
-    else {
-        array.push(userGuess);
-        document.getElementById(userGuess) = array;
+
+    // loop through the array and append to recorded guesses
+    // span tag or p tag or something
+
+    // empty your recorded guesses
+    recordedGuesses.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+        console.log('i: ', i);
+        let spanTag = document.createElement('span');
+        spanTag.innerText = i;
+        recordedGuesses.appendChild(spanTag);
     }
 
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
+    //userWins.textContent = "wins: " + wins;
+    //console.log("wins")
+    //userLosses.textContent = "losses: " + losses;
+    //guessesLeft.textContent = "Guesses Left: " + guesses;
+    //recordedGuesses.textContent = "Your guesses so far: " + guesses;
 
     
-
-
-
-}
+};
